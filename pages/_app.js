d65f4c09cfcd2fcf09520c2bdbecb88beb3793c9
Kamil <example@example.com>
2022-05-01 16:02:44 +0200

    add cart and payment

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
     <StateContext>
       <Layout>
         <Toaster />
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Layout>
     </StateContext>
   );
